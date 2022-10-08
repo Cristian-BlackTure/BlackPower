ca827568308c4caa5bef1fb7fc77a8500331e52f
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 16:24:00 +0300

    Update BlackBlocks.java

--- src/main/java/io/github/thebusybiscuit/ecopower/BlackBlocks.java
+++ src/main/java/io/github/thebusybiscuit/ecopower/BlackBlocks.java
@@ -32,10 +32,4 @@ public final class BlackBlocks {
             LoreBuilder.powerPerSecond(65_536),
             LoreBuilder.powerBuffer(65_536)
     );
-
-        new ReactorCore(REACTORCORE, new ItemStack[] {
-                SlimefunItems.SILVER_INGOT, null, null,
-                SlimefunItems.SILVER_INGOT, null, null,
-                SlimefunItems.SILVER_INGOT, null, null;
-        }).register(ecopower);
 }
