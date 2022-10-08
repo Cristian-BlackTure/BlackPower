59167aeba16dd79a19600c65febc0c357b31c345
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-08 15:54:34 +0300

    Create BlackBlocks.java

---
+++ src/main/java/io/github/thebusybiscuit/ecopower/BlackBlocks.java
@@ -0,0 +1,21 @@
+
+
+import lombok.experimental.UtilityClass;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineTier;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineType;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.utils.HeadTexture;
+import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
+
+/**
+ * Holds the base machines and setup
+ */
+@UtilityClass
+public final class BlackBlocks {
